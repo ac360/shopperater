0c5fd4976ed0bab3966485f1a8a7e53f8aee19c1
Austen Collins <austencollins@gmail.com>
2013-08-27 18:28:12 -0700

    changes

--- app/controllers/retailer_api_controller.rb
+++ app/controllers/retailer_api_controller.rb
@@ -1,5 +1,7 @@
 class RetailerApiController < ApplicationController
 
+	include ActionView::Helpers::NumberHelper
+
 	def product_search
 		client = A2z::Client.new(key: ENV["AMAZON_PAAPI_KEY"], secret: ENV["AMAZON_PAAPI_SECRET"], tag: ENV["AMAZON_PAAPI_TAG"])
 		# Because there are thousands of items in each search index, ItemSearch requires that you specify the value for at least one parameter in addition to a search index.
@@ -15,17 +17,22 @@ class RetailerApiController < ApplicationController
         # Create custom response here using the data from the Amazon API's reponse...
         @custom_search_results = []
         @results.items.each do |product|
-        	# product_price = check_for_price(product.offers.first.price.fractional)
-			@new_response = OpenStruct.new(
-	    					:id => product.asin, 
-	    					:source => "amazon", 
-	    					:title => product.title, 
-	    					:price => product.offers.first.price.fractional / 100.00, 
-	    					:img_small => product.medium_image.url, 
-	    					:img_big => product.large_image.url, 
-	    					:category =>  product.product_group, 
-	    					:link => "http://amazon.com/" )
-			@custom_search_results << @new_response
+
+			@new_response    		= 	OpenStruct.new(:source => "amazon")
+			@new_response.id 		= 	product.asin if product.asin.present? 
+			@new_response.title 	= 	product.title if product.title.present? 
+			if product.offers.present? 
+				@new_response.price = 	product.offers.first.price.fractional / 100.00
+				@new_response.price =   number_with_precision(@new_response.price, :precision => 2)
+			else
+				@new_response.price =   "No Price"
+			end
+			@new_response.img_small = 	product.medium_image.url if product.medium_image.present? 
+			@new_response.img_big   = 	product.large_image.url  if product.large_image.present?
+			@new_response.category  = 	product.product_group    if product.product_group.present?
+			@new_response.link      = 	"http://amazon.com/"
+
+			@custom_search_results  << @new_response
 		end
 	end
 
@@ -63,13 +70,5 @@ class RetailerApiController < ApplicationController
 				render :json => "The Retailer Parameter you included is incorrect or the Retailer is unsupported."
 		end
 	end # product_lookup
-
-	private
-
-	def check_for_price(price)
-
-
-	end
-
 
 end
