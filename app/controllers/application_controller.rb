fd8ccda980240fcdfa27f35cc19277ac9b9b019b
Austen Collins <austencollins@gmail.com>
2013-09-13 13:48:46 -0700

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout :layout_by_resource
 
+  def after_sign_in_path_for(resource) 
+    session["user_return_to"] || root
+  end
+
   private
 
   def layout_by_resource
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,11 @@
 class MainController < ApplicationController
 
 	def browser
+		session["user_return_to"] = request.url
 	end
 
 	def editor
+		session["user_return_to"] = request.url
 	end
 
 end
