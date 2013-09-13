1ace1debe5a896d4f7757081fc339eb3fc19ae27
Austen Collins <austencollins@gmail.com>
2013-09-13 13:52:33 -0700

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
 
   def after_sign_in_path_for(resource) 
-    session["user_return_to"] || root
+    session["user_return_to"] || root_path
+  end
+
+  def after_update_path_for(resource)
+    session["user_return_to"] || root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    session["user_return_to"] || root_path
   end
 
   private
