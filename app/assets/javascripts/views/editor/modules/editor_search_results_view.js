d542de945481a9f5c1e4d849f077967c3601e183
Austen Collins <austencollins@gmail.com>
2013-10-06 07:32:05 -0700

    Changes

--- app/assets/javascripts/views/editor/modules/editor_search_results_view.js
+++ app/assets/javascripts/views/editor/modules/editor_search_results_view.js
@@ -13,36 +13,6 @@ Medley.Views.EditorSearchResults = Backbone.View.extend({
 		"click #add-to-cart-button":           "addItemToCart"
     },
 
-    addItemToCart: function(e) {
-    	
-    	// If Guest Session
-    	var guestCartItems = $.jStorage.get("MedleyGuestCart");
-		if(!guestCartItems){
-			// Create Array w/ first Javascript Object
-			var guestCart = []
-			var cartItem1 = {}
-			// Get Product Details
-			cartItem1.title = $(e.currentTarget).attr( "data-title" );
-			cartItem1.asin = $(e.currentTarget).attr( "data-asin" );
-			cartItem1.product_group = $(e.currentTarget).attr( "data-product-group" );
-			guestCart.push(cartItem1)
-			// Save item to localstorage
-			$.jStorage.set("MedleyGuestCart", guestCart);
-			var newItemCount = guestCart.length;
-			$('#cart-item-count').text(newItemCount);
-		} else {
-			var guestCart = $.jStorage.get("MedleyGuestCart");
-			var newCartItem = {}
-			newCartItem.title = $(e.currentTarget).attr( "data-title" );
-			newCartItem.asin = $(e.currentTarget).attr( "data-asin" );
-			newCartItem.product_group = $(e.currentTarget).attr( "data-product-group" );
-			guestCart.push(newCartItem)
-			$.jStorage.set("MedleyGuestCart", guestCart);
-			var newItemCount = guestCart.length
-			$('#cart-item-count').text(newItemCount)
-		};
-    },
-
 	render: function () {
     this.$el.html(this.template({ collection: this.collection })).fadeIn(500);
     $('#module-product-results').niceScroll({
