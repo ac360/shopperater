ae9ff9c63bf8ddfa9bbe1d45aa62e22f04853b19
Austen Collins <austencollins@gmail.com>
2013-10-16 16:20:43 -0700

    changes

--- public/script/medleywidgets.js
+++ public/script/medleywidgets.js
@@ -6,11 +6,11 @@
 	  	global.mdlywidgets = {};
 	  	var MW = global.mdlywidgets;
 
-	  	var MW.el = Document.getElementById('mdlya1');
-	  	console.log(MW.el);
-	  	MW.el.style["background"] = "#000000";
-	  	MW.el.style["height"] = "100px";
-	  	MW.el.style["width"] = "200px";
+	  	var el = Document.getElementById('mdlya1');
+	  	console.log(el);
+	  	el.style["background"] = "#000000";
+	  	el.style["height"] = "100px";
+	  	el.style["width"] = "200px";
 
 	  	console.log("hello from the Medley Widgets Script");
 	 	
