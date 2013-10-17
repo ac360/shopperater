9c3fee2b315accc58c0a1d10f0a1f30da3e18895
Austen Collins <austencollins@gmail.com>
2013-10-17 14:55:26 -0700

    changes

--- app/controllers/api_external_controller.rb
+++ app/controllers/api_external_controller.rb
@@ -1,7 +1,12 @@
 class ApiExternalController < ApplicationController
 
 	def medley_show
-		@medley = Medley.find(params[:id])
+		@medley = Medley.where("id = ?", params[:id])
+		if !@medley.exists?
+			render :json => { :error => "This Medley Does Not Exist"}
+		else
+			@medley = @medley.first
+		end
 	end
 
 end
